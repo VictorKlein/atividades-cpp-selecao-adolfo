b18aac06efcb982e552793f29d57a98cdcc6b652
VictorKlein <93282488+VictorKlein@users.noreply.github.com>
2021-10-28 00:40:15 -0300

    Update Summation of primes.cpp

--- Summation of primes.cpp
+++ Summation of primes.cpp
@@ -3,18 +3,18 @@
 
 using namespace std;
 
-bool ehprimo[tam]; //Array que irá retornar 1 se o índice colocado é primo;
+bool ehprimo[tam]; 				//Array que irá retornar 1 se o índice colocado é primo;
 
 int main(){
 	long long soma = 0;
-	for (int i = 2; i < tam; i++) //Inicia em 2 porque 0 e 1 não são primos
+	for (int i = 2; i < tam; i++) 		//Inicia em 2 porque 0 e 1 não são primos
 		ehprimo[i] = 1;
 	for (long long i = 2; i < tam; i++){
-		if (ehprimo[i]){ 							//Sempre que encontrar um número primo:
+		if (ehprimo[i]){ 				//Sempre que encontrar um número primo:
 			for (long long j = i*i; j < tam; j += i){	//Nenhum múltiplo desse valor é primo, começando de i^2 porque todos
-				ehprimo[j] = 0;							//os múltiplos de i menores que i^2 já foram considerados para os primos anteriores
+				ehprimo[j] = 0;				//os múltiplos de i menores que i^2 já foram considerados para os primos anteriores
 			}
-	 	 	soma += i;                                  //Soma o valor encontrado ao total
+	 	 	soma += i;                                 	//Soma o valor encontrado ao total
 		}
 	}
 	cout<<"Resposta= "<<soma<<endl;
