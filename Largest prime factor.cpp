38ee88ee6be0d0660d3fa981a712739be4c3062b
VictorKlein <93282488+VictorKlein@users.noreply.github.com>
2021-10-28 01:35:29 -0300

    Update Largest prime factor.cpp

--- Largest prime factor.cpp
+++ Largest prime factor.cpp
@@ -6,15 +6,15 @@ int main(){
     long long numero = 600851475143;
     int i;
 
-
-    for (i=2; numero > 1; i++){
-        while (numero%i == 0){
-            numero/= i;
+                                //Algoritmo para fazer a fatoração:
+    for (i=2; numero > 1; i++){     //Enquanto ainda houver divisor de numero, para cada valor i...
+        while (numero%i == 0){      //Enquanto o número for divisível por i...
+            numero/= i;                 //Divida numero por i.
         }
-    }
-    i--;
+    }                           //Como, no final do for, i++ antes de verificar que numero = 1...
+    i--;                        //i-- para i ser igual ao último (maior) divisor primo de numero
 
-    cout<<i<<endl;
+    cout<<"Resposta: "<<i<<endl;
 
     return 0;
 }
