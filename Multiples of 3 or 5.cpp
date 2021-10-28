a68a79ac8622a2127c80040b3c6089b7f575cc02
VictorKlein <93282488+VictorKlein@users.noreply.github.com>
2021-10-28 01:27:54 -0300

    Update Multiples of 3 or 5.cpp

--- Multiples of 3 or 5.cpp
+++ Multiples of 3 or 5.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int main(){
-    long int soma=0;
-    for (int i=1; i<1000; i++){
-        if (i%3 == 0 || i%5 == 0){
-            soma += i;
+    int soma=0;
+    for (int i=1; i<1000; i++){         //Para cada valor de 1 a 1000...
+        if (i%3 == 0 || i%5 == 0){      //Se o valor for divisível por 3 ou por 5...
+            soma += i;                  //Soma o valor ao total
         }
     }
     cout<<soma<<endl;
