848038b03aef1b822e4cb43cc29db3fc4436d833
VictorKlein <93282488+VictorKlein@users.noreply.github.com>
2021-10-28 01:53:47 -0300

    Update Even Fibonacci numbers.cpp

--- Even Fibonacci numbers.cpp
+++ Even Fibonacci numbers.cpp
@@ -11,10 +11,10 @@ int main(){
     valor[0] = 1;
     valor[1] = 1;
     while(valor[1] < 4000000){
-        if (valor[1]%2 == 0)
-            soma += valor[1];
-        valor[1] = valor[1] + valor[0];
-        valor[0] = valor[1] - valor[0];
+        if (valor[1]%2 == 0)            
+            soma += valor[1];                   //Soma os valores pares
+        valor[1] = valor[1] + valor[0];     //O valor seguinte é a soma do valor atual com o do anterior
+        valor[0] = valor[1] - valor[0];     //O valor seguinte torna-se o anterior (valor[0] = valor[1] + valor[0] - valor[0])
     }
 
     cout<<soma<<endl;
