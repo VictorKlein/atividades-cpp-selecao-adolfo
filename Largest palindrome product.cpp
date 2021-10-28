58a7897b965374de1210955583221210497f01aa
VictorKlein <93282488+VictorKlein@users.noreply.github.com>
2021-10-28 01:45:14 -0300

    Update Largest palindrome product.cpp

--- Largest palindrome product.cpp
+++ Largest palindrome product.cpp
@@ -3,36 +3,36 @@
 using namespace std;
 
 int numeros[1000000];
-bool eh_palindromo(int num){
-	int aux = num, inverso=0;
-	while(aux){       
-		inverso *= 10;
-		inverso += aux%10;
-		aux /= 10;
+bool eh_palindromo(int num){		//Verifica se num é palíndromo:
+	int aux = num, inverso=0;		//aux = num
+	while(aux){       			//Enquanto restarem dígitos no em aux...
+		inverso *= 10;				//Cria um espaço (à direita) para mais um algarismo no número inverso...
+		inverso += aux%10;			//Adiciona o próximo dígito de aux a inverso 
+		aux /= 10;				//Remove de aux o dígito que foi adicionado a inverso
 	}
-	return inverso == num;
+	return inverso == num;			//Compara o número original com sua versão invertida
 }
 
 int main(){
     int aux, pos=0, resposta=0;
-    
-    for (int i=0; i <= 999 ; i++){             //Começa de 999x999 e calcula todas as possibilidades até 991X991
-        for (int j = 0; j <= 999 ; j++){       //Depois, vai de 989x989 até 981x981
-            aux = i*j;
-            if (eh_palindromo(aux)){
-                numeros[pos] = aux;
+    						
+    for (int i=0; i <= 999 ; i++){             
+        for (int j = 0; j <= 999 ; j++){       //Para cada par de valores 0 <= i, j <= 1000...
+            aux = i*j;					//Obtém o produto dos dois
+            if (eh_palindromo(aux)){			//Se o produto for palíndromo,
+                numeros[pos] = aux;				//Adiciona o valor a um array que armazena todos os palíndromos encontrados
                 pos++;
             }
         }
-    }
-    for (int j = 0; j < pos; j++){
+    }						//Para encontrar o maior valor palíndromo armazenado
+    for (int j = 0; j < pos; j++){		
         if (resposta < numeros[j])
             resposta = numeros[j];
     }
-    if (resposta){
+    if (resposta){				//Se encontrar uma resposta, imprime
         cout<<"resposta: "<<resposta<<endl;
         return 0;
-    }
-    cout<<"falhou"<<endl;
+    }					
+    cout<<"falhou"<<endl;			//Para debugging
     return 0;
 }
